7b66bafcce1a3c202cf31d6a1efe73f9902f9800
Akerus <37414159+Takeces@users.noreply.github.com>
2024-03-22 11:26:37 +0100

    Create PokeClicker SoftReset Buy Shop Pokemon.js

---
+++ PokeClicker SoftReset Buy Shop Pokemon.js
@@ -0,0 +1,108 @@
+// ==UserScript==
+// @name         PokeClicker SoftReset Buy Shop Pokemon
+// @namespace    pcInfoStuff
+// @version      0.1
+// @description  Soft reset for buying shop pokemon
+// @author       Takeces
+// @match        https://www.pokeclicker.com/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    function init() {
+        Game.prototype.save = function() {};
+        checkAndLoadSave();
+    }
+
+    function checkAndLoadSave() {
+        let elems = document.querySelectorAll('.trainer-card');
+        if(elems.length === 0) {
+            setTimeout(checkAndLoadSave, 250);
+            return;
+        }
+        elems[0].click();
+        setTimeout(checkGameLoadedAndRunning, 250);
+    }
+
+    function checkGameLoadedAndRunning() {
+        let elem = document.getElementById('battleContainer');
+        if(elem === null) {
+            setTimeout(checkGameLoadedAndRunning, 250);
+            return;
+        }
+        if (!elem.checkVisibility()) {
+            setTimeout(checkGameLoadedAndRunning, 250);
+            return;
+        }
+
+        setTimeout(doSoftResetStuff, 250);
+    }
+
+    function doSoftResetStuff() {
+        autoLoop();
+    }
+
+    function autoLoop() {
+        if(doTheThing()) {
+            // if 'true' was returned, a reload has been issued -> don't want to run the script yet again, or it might save
+            return;
+        }
+        setTimeout(autoLoop, 25);
+    }
+
+    function doTheThing() {
+        const shops = player.town().content.filter(c => c instanceof Shop);
+        if (player.route() || !shops.length) {
+            return;
+        }
+
+        const items = shops.map(shop => shop.items.filter(i => i instanceof PokemonItem && !App.game.party.alreadyCaughtPokemonByName(i.name, true)));
+
+        for(let indexShop = 0; indexShop < items.length; indexShop++) {
+            if(items[indexShop].length === 0) { continue; }
+            ShopHandler.showShop(shops[indexShop]);
+            for(let indexItem = 0; indexItem < items[indexShop].length; indexItem++) {
+                ShopHandler.setSelected(shops[indexShop].items.indexOf(items[indexShop][indexItem]));
+                let amount = 1;
+                // if the needed currency is pokedollars, buy 10 at once
+                if(items[indexShop][indexItem].currency === GameConstants.Currency.money) {
+                    amount = 10;
+                }
+                ShopHandler.increaseAmount(amount - 1);
+                if (App.game.wallet.hasAmount(new Amount(items[indexShop][indexItem].totalPrice(ShopHandler.amount()), items[indexShop][indexItem].currency))) {
+                    ShopHandler.buyItem();
+                    if(App.game.party.alreadyCaughtPokemonByName(items[indexShop][indexItem].name, true)) {
+                        saveGame();
+                        return;
+                    }
+
+                    // not been shiny -> reload
+                    doReload();
+                    return true;
+                }
+            }
+        }
+    }
+
+    function saveGame() {
+        Save.store(player);
+    }
+
+    function loadSave() {
+        document.querySelectorAll('.trainer-card')[0].click();
+    }
+
+    function doReload() {
+        location.reload();
+    }
+
+	/** Basic initialization call */
+
+    if(document.readyState === 'complete' || document.readyState === 'interactive') {
+        setTimeout(init, 1);
+    } else {
+        document.addEventListener('DOMContentLoaded', init);
+    }
+})();
