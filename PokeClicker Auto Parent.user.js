57a10e2c176a0fc4625f261dea46532199df9ebe
Akerus <37414159+Takeces@users.noreply.github.com>
2024-03-28 09:46:01 +0100

    Update and rename PokeClicker Auto Parent.js to PokeClicker Auto Parent.user.js

--- PokeClicker Auto Parent.user.js
+++ PokeClicker Auto Parent.user.js
@@ -1,9 +1,11 @@
 // ==UserScript==
 // @name         PokeClicker Auto Parent
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.2
 // @description  Parent with Container for all the auto stuff
 // @author       Takeces
+// @updateURL	 https://github.com/Takeces/PokeclickerScripts/raw/main/PokeClicker%20Auto%20Parent.js
+// @downloadURL	 https://github.com/Takeces/PokeclickerScripts/raw/main/PokeClicker%20Auto%20Parent.js
 // @match        https://www.pokeclicker.com/*
 // @grant        none
 // ==/UserScript==
