921c76b162dd91665f3c72a00c52287f64b0ddbc
Akerus <37414159+Takeces@users.noreply.github.com>
2024-04-11 15:24:19 +0200

    Update and rename PokeClicker Open More Instances.js to PokeClicker Auto Load Save.js

---
+++ PokeClicker Auto Load Save.js
@@ -0,0 +1,32 @@
+// ==UserScript==
+// @name         PokeClicker Auto Load Save
+// @namespace    pcInfoStuff
+// @version      0.1
+// @description  open more windows
+// @author       Takeces
+// @match        https://www.pokeclicker.com/*
+// @updateURL	 https://raw.githubusercontent.com/Takeces/PokeclickerScripts/main/PokeClicker%20Auto%20Load%20Save.js
+// @downloadURL	 https://raw.githubusercontent.com/Takeces/PokeclickerScripts/main/PokeClicker%20Auto%20Load%20Save.js
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    function init() {
+        let elems = document.querySelectorAll('.trainer-card');
+        if(elems.length === 0) {
+            setTimeout(init, 250);
+            return;
+        }
+        elems[0].click();
+    }
+
+	/** Basic initialization call */
+
+    if(document.readyState === 'complete' || document.readyState === 'interactive') {
+        setTimeout(init, 1);
+    } else {
+        document.addEventListener('DOMContentLoaded', init);
+    }
+})();
