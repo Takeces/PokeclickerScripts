c2687b1c28e48bcc2f19d25b0744d88eb2c130da
Akerus <37414159+Takeces@users.noreply.github.com>
2024-05-03 14:55:42 +0200

    Update PokeClicker Auto Farm.user.js

--- PokeClicker Auto Farm.user.js
+++ PokeClicker Auto Farm.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         PokeClicker Auto Farm
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.2
 // @description  Auto farming
 // @author       Takeces
 // @updateURL	 https://github.com/Takeces/PokeclickerScripts/raw/main/PokeClicker%20Auto%20Farm.user.js
