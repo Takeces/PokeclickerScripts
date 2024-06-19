712ac947b6d20268f2515d98d9f1c4b071c6ee28
Akerus <37414159+Takeces@users.noreply.github.com>
2024-06-19 20:48:12 +0200

    Update PokeClicker Auto Load Save.user.js

--- PokeClicker Auto Load Save.user.js
+++ PokeClicker Auto Load Save.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         PokeClicker Auto Load Save
 // @namespace    pcInfoStuff
-// @version      0.2
+// @version      0.3
 // @description  open more windows
 // @author       Takeces
 // @match        https://www.pokeclicker.com/*
@@ -20,6 +20,61 @@
             return;
         }
         elems[0].click();
+        setTimeout(checkGameLoadedAndRunning, 250);
+    }
+
+    function checkGameLoadedAndRunning() {
+        let elem = document.getElementById('battleContainer');
+        if(elem === null) {
+            setTimeout(checkGameLoadedAndRunning, 250);
+            return;
+        }
+        if (!elem.checkVisibility()) {
+            setTimeout(checkGameLoadedAndRunning, 250);
+            return;
+        }
+
+        setTimeout(doStuffAfterLoading, 250);
+    }
+
+    function doStuffAfterLoading() {
+        let PcAutomationHolder = window.PcAutomationHolder;
+        if(!PcAutomationHolder) {
+            setTimeout(doStuffAfterLoading, 250);
+            return;
+        }
+
+/*         if(!PcAutomationHolder.dungeonRunner.toggleAutoStartDungeon) {
+            setTimeout(doStuffAfterLoading, 250);
+            return;
+        }
+        PcAutomationHolder.dungeonRunner.toggleAutoStartDungeon(); */
+
+/*         if(!PcAutomationHolder.dungeonRunner.toggleAutoDungeon) {
+            setTimeout(doStuffAfterLoading, 250);
+            return;
+        }
+        PcAutomationHolder.dungeonRunner.toggleAutoDungeon();
+        PcAutomationHolder.dungeonRunner.toggleAllTiles();
+        //PcAutomationHolder.dungeonRunner.toggleBossRush(); */
+
+        if(!PcAutomationHolder.toggleAutoUnderground) {
+            setTimeout(doStuffAfterLoading, 250);
+            return;
+        }
+        PcAutomationHolder.toggleAutoUnderground();
+
+        if(!PcAutomationHolder.toggleAutoBreed) {
+            setTimeout(doStuffAfterLoading, 250);
+            return;
+        }
+        PcAutomationHolder.toggleAutoBreed();
+
+        if(!PcAutomationHolder.toggleAutoFarm) {
+            setTimeout(doStuffAfterLoading, 250);
+            return;
+        }
+        PcAutomationHolder.toggleAutoFarm();
     }
 
 	/** Basic initialization call */
