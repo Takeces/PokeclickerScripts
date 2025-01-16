49abaeffdf6a131008137dbdb8543f2993abeda1
Akerus <37414159+Takeces@users.noreply.github.com>
2025-01-16 14:53:03 +0100

    Update PokeClicker SoftReset Breeding Eggs.user.js

--- PokeClicker SoftReset Breeding Eggs.user.js
+++ PokeClicker SoftReset Breeding Eggs.user.js
@@ -4,8 +4,6 @@
 // @version      0.3
 // @description  Soft reset for breeding eggs
 // @author       Takeces
-// @updateURL	 https://github.com/Takeces/PokeclickerScripts/raw/main/PokeClicker%20SoftReset%20Breeding%20Eggs.user.js
-// @downloadURL	 https://github.com/Takeces/PokeclickerScripts/raw/main/PokeClicker%20SoftReset%20Breeding%20Eggs.user.js
 // @match        https://www.pokeclicker.com/*
 // @grant        none
 // ==/UserScript==
