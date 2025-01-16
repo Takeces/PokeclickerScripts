25a12a122e1c09e5f7945ad3997dfe5d4dd665fe
Akerus <37414159+Takeces@users.noreply.github.com>
2025-01-16 14:50:46 +0100

    Update PokeClicker Auto Farm Unlocks.user.js

--- PokeClicker Auto Farm Unlocks.user.js
+++ PokeClicker Auto Farm Unlocks.user.js
@@ -4,8 +4,6 @@
 // @version      0.7
 // @description  Auto unlock berries in farm
 // @author       Takeces
-// @updateURL	 https://github.com/Takeces/PokeclickerScripts/raw/main/PokeClicker%20Auto%20Farm%20Unlocks.user.js
-// @downloadURL	 https://github.com/Takeces/PokeclickerScripts/raw/main/PokeClicker%20Auto%20Farm%20Unlocks.user.js
 // @match        https://www.pokeclicker.com/*
 // @grant        none
 // ==/UserScript==
