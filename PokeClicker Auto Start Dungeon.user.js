c755c2a4202525e27d66bc52a8ebca2b65764328
Akerus <37414159+Takeces@users.noreply.github.com>
2025-01-16 14:33:48 +0100

    Update PokeClicker Auto Start Dungeon.user.js

--- PokeClicker Auto Start Dungeon.user.js
+++ PokeClicker Auto Start Dungeon.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         PokeClicker Auto Start Dungeon
 // @namespace    http://tampermonkey.net/
-// @version      0.5
+// @version      0.6
 // @description  Auto start dungeons
 // @author       Takeces
 // @updateURL	 https://github.com/Takeces/PokeclickerScripts/raw/main/PokeClicker%20Auto%20Start%20Dungeon.user.js
@@ -18,6 +18,7 @@
     const BUTTON_CAUGHT_ID = 'pcDoAutoStartDungeonCaught';
     const BUTTON_SHINY_ID = 'pcDoAutoStartDungeonShiny';
     const BUTTON_ACHIEVEMENT_ID = 'pcDoAutoStartDungeonAchievement';
+    const BUTTON_PKRS_ID = 'pcDoAutoStartDungeonPkrs';
 
     function init() {
         let PcAutomationHolder = window.PcAutomationHolder;
@@ -33,23 +34,33 @@
 
         var btnCaught = document.createElement('button');
         btnCaught.setAttribute('id', BUTTON_CAUGHT_ID);
-        btnCaught.innerHTML = 'DC';
+        btnCaught.innerHTML = 'C';
+        btnCaught.title = 'until all caught';
         btnCaught.addEventListener('click', toggleCaught);
 
         var btnCaughtShiny = document.createElement('button');
         btnCaughtShiny.setAttribute('id', BUTTON_SHINY_ID);
-        btnCaughtShiny.innerHTML = 'DS';
+        btnCaughtShiny.innerHTML = 'S';
+        btnCaughtShiny.title = 'until all shiny';
         btnCaughtShiny.addEventListener('click', toggleShiny);
 
         var btnAchievement = document.createElement('button');
         btnAchievement.setAttribute('id', BUTTON_ACHIEVEMENT_ID);
-        btnAchievement.innerHTML = 'DA';
+        btnAchievement.innerHTML = 'A';
+        btnAchievement.title = 'until all achievements';
         btnAchievement.addEventListener('click', toggleAchievement);
+
+        var btnPkrs = document.createElement('button');
+        btnPkrs.setAttribute('id', BUTTON_PKRS_ID);
+        btnPkrs.innerHTML = 'PKRS';
+        btnPkrs.title = 'until all resistant';
+        btnPkrs.addEventListener('click', togglePkrs);
 
         PcAutomationHolder.addAutomationButton(btn, true);
         PcAutomationHolder.addAutomationButton(btnCaught, true);
         PcAutomationHolder.addAutomationButton(btnCaughtShiny, true);
         PcAutomationHolder.addAutomationButton(btnAchievement, true);
+        PcAutomationHolder.addAutomationButton(btnPkrs, true);
 
         if(!PcAutomationHolder.dungeonRunner) {
             PcAutomationHolder.dungeonRunner = {};
@@ -62,6 +73,7 @@
 	var autoStartUntilAllCaught = false;
 	var autoStartUntilAllCaughtShiny = false;
 	var autoStartUntilAchievementFull = false;
+	var autoStartUntilResistant = false;
 
 	function startDungeon() {
 		// not in a "town"
@@ -83,6 +95,12 @@
 		if(autoStartUntilAchievementFull && DungeonRunner.isAchievementsComplete(player.town.dungeon)) {
 			return;
 		}
+
+        if(autoStartUntilResistant
+           && (RouteHelper.minPokerus(player.town.dungeon.allAvailablePokemon()) >= GameConstants.Pokerus.Resistant
+               || RouteHelper.minPokerus(player.town.dungeon.allAvailablePokemon()) == GameConstants.Pokerus.Uninfected)) {
+           return;
+        }
 
 		DungeonRunner.initializeDungeon(player.town.dungeon);
 	}
@@ -135,6 +153,16 @@
 		autoStartUntilAchievementFull = true;
         document.getElementById(BUTTON_ACHIEVEMENT_ID).style.backgroundColor = 'green';
 	}
+
+    function togglePkrs() {
+		if(autoStartUntilResistant) {
+			autoStartUntilResistant = false;
+            document.getElementById(BUTTON_PKRS_ID).style.backgroundColor = '';
+			return;
+		}
+		autoStartUntilResistant = true;
+        document.getElementById(BUTTON_PKRS_ID).style.backgroundColor = 'green';
+    }
 
 	/** Basic initialization call */
 
