cb5b951720d4f2bddcac9d5449bf3d367c30866c
Akerus <37414159+Takeces@users.noreply.github.com>
2025-01-16 14:50:18 +0100

    Update PokeClicker Auto Breeding.user.js

--- PokeClicker Auto Breeding.user.js
+++ PokeClicker Auto Breeding.user.js
@@ -4,8 +4,6 @@
 // @version      0.4
 // @description  Auto Breeding
 // @author       Takeces
-// @updateURL	 https://github.com/Takeces/PokeclickerScripts/raw/main/PokeClicker%20Auto%20Breeding.user.js
-// @downloadURL	 https://github.com/Takeces/PokeclickerScripts/raw/main/PokeClicker%20Auto%20Breeding.user.js
 // @match        https://www.pokeclicker.com/*
 // @grant        none
 // ==/UserScript==
